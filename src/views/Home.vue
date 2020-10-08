94a84eb136e716d18664a9acf7fb6b69911aa158
Hanqing1996 <zhq199622@163.com>
2020-10-08 20:05:44 +0800

    更新 topNav 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <div class="topNav">
-            <div class="logo"></div>
-            <div class="menu"></div>
+            <div class="logo">LOGO</div>
+            <ul class="menu">
+                <li>菜单1</li>
+                <li>菜单2</li>
+            </ul>
         </div>
         <div class="banner">
             <h1>superUI</h1>
@@ -21,6 +24,22 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .topNav {
+        background: pink;
+        display: flex;
+        padding: 16px;
+        > .logo {
+            max-width: 6em;
+            margin-right: auto;
+        }
+        > .menu {
+            display: flex;
+            white-space: nowrap;
+            flex-wrap: nowrap;
+            > li {
+                margin: 0 1em;
+            }
+        }
+    }
 </style>
